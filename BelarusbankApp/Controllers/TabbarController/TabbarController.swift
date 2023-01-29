f9420ae2c2099be0df6f4606a15d4f91a7e29fe5
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-01-29 19:26:30 +0300

    TabbarController Setup

--- BelarusbankApp/Controllers/TabbarController/TabbarController.swift
+++ BelarusbankApp/Controllers/TabbarController/TabbarController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class TabbarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configurateTabBar()
+        self.tabBar.backgroundColor = .darkGray
+        self.tabBar.tintColor = .green
+        self.tabBar.layer.opacity = 0.6
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configurateTabBar() {
+        let mapVC = MapController(nibName: String(describing: MapController.self), bundle: nil)
+        let gemsVC = GemsController(nibName: String(describing: GemsController.self), bundle: nil)
+        let ingotsVC = IngotsController(nibName: String(describing: IngotsController.self), bundle: nil)
+        let settingsVC = SettingsController(nibName: String(describing: SettingsController.self), bundle: nil)
+        
+        self.viewControllers = [mapVC, gemsVC, ingotsVC, settingsVC]
+        mapVC.tabBarItem = UITabBarItem(title: "Карта", image: UIImage(systemName: "map"), tag: 0)
+        gemsVC.tabBarItem = UITabBarItem(title: "Алмазы", image: UIImage(systemName: "pentagon"), tag: 1)
+        ingotsVC.tabBarItem = UITabBarItem(title: "Слитки", image: UIImage(systemName: "rectangle.center.inset.fill"), tag: 2)
+        settingsVC.tabBarItem = UITabBarItem(title: "Настройки", image: UIImage(systemName: "gear"), tag: 3)
     }
-    */
-
+    
 }
