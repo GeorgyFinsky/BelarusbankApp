5b5993515dd47db3b8a23a1880910898af2bc5d8
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-01-29 18:39:26 +0300

    BankProvider Created

--- BelarusbankApp/Providers/BankProvider.swift
+++ BelarusbankApp/Providers/BankProvider.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import Moya
+
+typealias ArrayResponce<T: Decodable> = (([T]) -> Void)
+typealias ObjectResponce<T: Decodable> = ((T) -> Void)
+typealias Error = ((String) -> Void)
+
+final class BankFacilityProvider {
+    private let provider = MoyaProvider<BankAPI>(plugins: [NetworkLoggerPlugin()])
+    
+    func getATMs(success: @escaping ArrayResponce<ATMModel>, failure: @escaping Error) {
+        provider.request(.getAtm) { result in
+            switch result {
+                case .success(let responce):
+                    guard let result = try? JSONDecoder().decode([ATMModel].self, from: responce.data) else { return }
+                    success(result)
+                case .failure(let error):
+                    failure(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getDepartments(success: @escaping ArrayResponce<DepartmentModel>, failure: @escaping Error) {
+        provider.request(.getDepartments) { result in
+            switch result {
+                case .success(let responce):
+                    guard let result = try? JSONDecoder().decode([DepartmentModel].self, from: responce.data) else { return }
+                    success(result)
+                case .failure(let error):
+                    failure(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getGems(success: @escaping ArrayResponce<GemModel>, failure: @escaping Error) {
+        provider.request(.getGems) { result in
+            switch result {
+                case .success(let responce):
+                    guard let result = try? JSONDecoder().decode([GemModel].self, from: responce.data) else { return }
+                    success(result)
+                case .failure(let error):
+                    failure(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getIngots(success: @escaping ArrayResponce<IngotModel>, failure: @escaping Error) {
+        provider.request(.getIngots) { result in
+            switch result {
+                case .success(let responce):
+                    guard let result = try? JSONDecoder().decode([IngotModel].self, from: responce.data) else { return }
+                    success(result)
+                case .failure(let error):
+                    failure(error.localizedDescription)
+            }
+        }
+    }
+
+}
