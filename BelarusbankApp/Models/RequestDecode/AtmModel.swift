6da25a68cf9621291d49eed07914442fe554116f
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-01-29 18:25:15 +0300

    ATMModel Created

--- BelarusbankApp/Models/RequestDecode/AtmModel.swift
+++ BelarusbankApp/Models/RequestDecode/AtmModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct ATMModel: Decodable, BankFacility {
+    var id: String = ""
+    var gpsX: String = ""
+    var gpsY: String = ""
+    var city: String = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case gpsX = "gps_x"
+        case gpsY = "gps_y"
+        case city = "city"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        gpsX = try container.decode(String.self, forKey: .gpsX)
+        gpsY = try container.decode(String.self, forKey: .gpsY)
+        city = try container.decode(String.self, forKey: .city)
+    }
+    
+}
