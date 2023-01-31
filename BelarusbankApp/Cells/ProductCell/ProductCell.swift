eff3adaee75b485e4a48e7c52fa9b82d9fd3bce3
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-01-31 12:30:31 +0300

    productCell ingot set update

--- BelarusbankApp/Cells/ProductCell/ProductCell.swift
+++ BelarusbankApp/Cells/ProductCell/ProductCell.swift
@@ -15,7 +15,6 @@ class ProductCell: UITableViewCell {
     @IBOutlet weak var info2Label: UILabel!
     @IBOutlet weak var info3Label: UILabel!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -30,9 +29,9 @@ class ProductCell: UITableViewCell {
     
     func set(ingot: DepartmentIngots) {
         self.departmentIDLabel.text = "Отделение №\(ingot.departmentID)"
-        self.info1Label.text = "10 гр - \(ingot.ingotsPrices[0].pricePer10gr) BYN"
-        self.info2Label.text = "20 гр - \(ingot.ingotsPrices[0].pricePer20gr) BYN"
-        self.info3Label.text = "50 гр - \(ingot.ingotsPrices[0].pricePer50gr) BYN"
+        self.info1Label.text = ingot.ingotsPrices[0].pricePer10gr != "0.00" ? "10 гр - \(ingot.ingotsPrices[0].pricePer10gr) BYN" : "10 гр - Нет в продаже"
+        self.info2Label.text = ingot.ingotsPrices[0].pricePer20gr != "0.00" ? "20 гр - \(ingot.ingotsPrices[0].pricePer20gr) BYN" : "20 гр - Нет в продаже"
+        self.info3Label.text = ingot.ingotsPrices[0].pricePer50gr != "0.00" ? "50 гр - \(ingot.ingotsPrices[0].pricePer50gr) BYN" : "50 гр - Нет в продаже"
     }
     
 }
